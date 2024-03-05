4bac2d486e22d23c7d63ccbc9785fcfbb64168e0
Aakash Sharma <110731220+aakashnnt123@users.noreply.github.com>
2024-03-05 14:45:07 +0000

    container config done

---
+++ 01_basis/text.js
@@ -0,0 +1,82 @@
+console.log("HYY AAKASH")
+
+// VARIABLES......
+
+// const accountID=144553
+// let accountEmail="aakash123@gnail.com"
+// var accountpassward ="12345"
+// CITY="jaipur"
+// let accountstate;
+
+// console.log(accountID)
+
+// console.table([accountID,accountEmail,accountpassward,accountstate])
+
+//DATA TYPES.........
+
+// let name="aakash"
+// let age=18
+// let isloggedin=false
+
+
+//number=2 to power 53
+//bigint 
+//string= ""
+//boolean = true/false
+//null=standalone value
+//undefined=>
+//symbol=>unique
+
+//object
+
+// console.log(typeof age)
+// console.log(typeof null)
+// console.log(typeof undefined)
+
+//CONVERSION OPERATIONS........\
+
+// let score="33"
+// let score1="33abc"
+
+// console.log(typeof score);
+// console.log(typeof score1);
+
+// let number = Number(score);
+// let number1 = Number(score1);
+
+// console.log(typeof number);
+// console.log(typeof number1);
+
+
+//OPERATIONS..........................
+
+// let value=3
+// let negValue=-value
+// console.log(negValue)
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**2);
+// console.log(2/2);
+// console.log(2%2);
+
+// let str="hello"
+// let str2="aakash"
+
+// let str3=str +" "+ str2
+// console.log(str3)
+// console.log("1"+2);
+// console.log(1+"2")
+// console.log(1+"2"+"2")
+// console.log(1+2+"2")
+// console.log("1"+2+2)
+
+
+
+
+
+
+
+
+
