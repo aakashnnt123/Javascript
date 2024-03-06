dd11c1eb9c7875bbb2f9b1fa4593861b32f59286
Aakash Sharma <110731220+aakashnnt123@users.noreply.github.com>
2024-03-06 17:43:53 +0000

    commit

--- 01_basis/text.js
+++ 01_basis/text.js
@@ -77,6 +77,13 @@
 // console.log(1+2+"2")
 // console.log("1"+2+2)
 
+// console.log(+true)
+// console.log(+"")
+
+// let gamecounter = 100;
+// gamecounter++;
+// console.log(gamecounter)
+
 
 
 
