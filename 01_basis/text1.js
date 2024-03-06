091f716229de76e19ac703e4ae531131782437b3
Aakash Sharma <110731220+aakashnnt123@users.noreply.github.com>
2024-03-06 18:28:10 +0000

    commit

---
+++ 01_basis/text1.js
@@ -0,0 +1,117 @@
+// console.log(2>1);
+// console.log(2<1);
+// console.log(2>=1);
+// console.log(2==1);
+
+
+// console.log("2">1);
+// console.log("02">1);
+// console.log(null>0);
+// console.log(null==0);
+// console.log(null>=0);
+
+// DATA TYPES...
+
+
+/*PRIMITIVE  DATA TYPE..... --(IT IS CALL BY VALUE)
+
+...7 TYPES --
+SRTING,NUMBER,BOOLEAN,NULL UNDEFINED,SYMBOL.BIGINT..*/
+  
+// const id = Symbol('123')
+// const anotherid = Symbol('123')
+
+// console.log(id)
+// console.log(anotherid)
+// console.log(id == anotherid)
+
+
+/*REFERENCE(NON PRIMITIVE)..
+
+--ARRAY,OBJECTS,FUNCATIONS...*/
+
+// const heros=["shaktiman","naaraj","doga"]
+// let myobj = {
+//     name:"aakash",
+//     age:22,
+// }
+// console.log(myobj)
+
+// const myFunction = function()
+// {
+//     console.log("HELLO WORLD");
+// }
+
+// console.log(myFunction()); 
+
+console.log(typeof BigInt);
+console.log(typeof myFunction);
+console.log(typeof null);
+
+//=====-------------------===============
+
+// stack memory(primitive)--give copy of original value
+//heap(non-primitive)-give access/reference of original value
+
+// let myYouTube="aakashcoding"
+// let anothername=myYouTube
+
+// anothername="chai aur code"
+
+
+// console.log(anothername)
+// console.log(myYouTube)
+
+
+// let user ={
+//     email:"user@gnail.com",
+//     upi:"87386356",
+// }
+//  let usertwo=user
+//  usertwo.email="aakash@gmail.com"
+
+//  console.log(user)
+//  console.log(usertwo)
+
+
+// .................................STRING......................................
+
+    
+// const name="Aakash"
+// const repocount=50
+
+// console.log(name+" "+repocount+"value")
+
+// console.log(`hello my name is ${name} and my repo count is ${repocount}`)
+
+// const gamename = new String('aakashnnt')
+
+// console.log(gamename[0]);
+
+// console.log(gamename.__proto__);
+
+// console.log(gamename.length)
+// console.log(gamename.toUpperCase());
+// console.log(gamename.charAt(2));
+// console.log(gamename.indexOf('a'));
+// console.log(gamename.substring(0,4));
+// console.log(gamename.slice(1,4));
+
+// const newStringOne="   aakash    "
+// console.log(newStringOne);
+// console.log(newStringOne.trim());
+
+
+// const url = "http://aakash.com/aakash%20sharma";
+
+// console.log(url.replace('%20','-'))
+// console.log(url.includes('aakash'))
+
+// console.log(gamename.split('-'))
+
+
+
+
+
+
+
